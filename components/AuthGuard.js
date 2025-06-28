8d07fb554c1cd3b0b31659bdf6b8f4c7a2f372d7
SkhumbuzoT <109624354+SkhumbuzoT@users.noreply.github.com>
2025-06-29 00:41:31 +0200

    Update AuthGuard.js

--- components/AuthGuard.js
+++ components/AuthGuard.js
@@ -1,20 +1,4 @@
-import { useRouter } from 'next/router';
-import { useAuth } from '../context/AuthContext';
-import { useEffect } from 'react';
-
 export default function AuthGuard({ children }) {
-  const { user, loading } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!loading && !user) {
-      router.push('/');
-    }
-  }, [user, loading, router]);
-
-  if (loading || !user) {
-    return <div>Loading...</div>;
-  }
-
+  // Remove authentication check
   return children;
 }
